60b5d8683543c66838a18ae4a35ac5bc3aa66a9a
winter <winter@87-77-177-188.mna.fu-berlin.de>
2023-07-25 08:16:20 +0200

    Initial commit

--- weibo/settings.py
+++ weibo/settings.py
@@ -12,7 +12,7 @@ DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': 'login_sid_t=02348f40c7aa26a882ad3e330eeee6b8; cross_origin_proto=SSL; _s_tentry=weibo.com; Apache=7492173878339.592.1688827071113; SINAGLOBAL=7492173878339.592.1688827071113; ULV=1688827071117:1:1:1:7492173878339.592.1688827071113:; SUB=_2A25JuujkDeRhGeNM7lEV9SbOzz-IHXVqzl0srDV8PUNbmtAbLVenkW9NTgX5tyqnO5hf5klT-sSaYkziBgpgT6R7; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5BGXPPCwhe5c4iuG2_AsFA5JpX5KzhUgL.Fo-ESKeXSKnEShe2dJLoI7vhdcf7dsLoqPxL9FfuIgBt; ALF=1721748531; SSOLoginState=1690212533; ab.storage.deviceId.a9882122-ac6c-486a-bc3b-fab39ef624c5=%7B%22g%22%3A%22f64628ef-482d-0a5a-0964-816e30efbce3%22%2C%22c%22%3A1690212571770%2C%22l%22%3A1690212571770%7D'
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,7 +24,7 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['经济抵制']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 1
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
@@ -33,12 +33,12 @@ CONTAIN_TYPE = 0
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用“全部”
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-03-01'
+START_DATE = '2022-02-23'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2020-03-01'
+END_DATE = '2023-03-23'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
-FURTHER_THRESHOLD = 46
+RANDOMIZE_DOWNLOAD_DELAY = True
 # 图片文件存储路径
 IMAGES_STORE = './'
 # 视频文件存储路径
